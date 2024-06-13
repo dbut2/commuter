8388d021f590d9b9dabf5526fa69ffa025854561
Dylan Butler <dylan@but.la>
2024-06-13 10:49:32 +1000

    main

--- generate.go
+++ generate.go
@@ -1,3 +1,3 @@
 package commuter
 
-//go:generate swagger-codegen generate --input-spec https://developers.strava.com/swagger/swagger.json --lang go --output scr/strava-api
+//go:generate swagger-codegen generate --input-spec https://developers.strava.com/swagger/swagger.json --lang go --output src/strava-api --additional-properties packageName=strava-api
