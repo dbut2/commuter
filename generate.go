546c9424a265ecfa22a878648460553a671ce8d5
Dylan Butler <dylan@but.la>
2024-06-13 10:32:48 +1000

    dev

---
+++ generate.go
@@ -0,0 +1,3 @@
+package commuter
+
+//go:generate swagger-codegen generate --input-spec https://developers.strava.com/swagger/swagger.json --lang go --output scr/strava-api
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/dbut2/commuter
+
+go 1.22.4
