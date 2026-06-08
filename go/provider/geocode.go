1f421a2f56ea9c163a235c461f9178d2b38b17f7
Dylan Butler <dylan@but.la>
2026-06-08 23:04:44 +0900

    lint

--- go/provider/geocode.go
+++ go/provider/geocode.go
@@ -172,7 +172,7 @@ func geocodeFetch(ctx context.Context, lat, lon float64, zoom int) (json.RawMess
 	if err != nil {
 		return nil, err
 	}
-	defer resp.Body.Close()
+	defer func() { _ = resp.Body.Close() }()
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return nil, err
